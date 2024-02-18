83734218077492884917ce1e33a06556eb7d8730
Oleksandr Savelev <a.savelev.krm@gmail.com>
2024-02-18 15:51:07 +0200

    add static generation posts pages with using scully

---
+++ mdfiles/angular-and-scully.md
@@ -0,0 +1,7 @@
+---
+title: 'Angular and Scully'
+description: 'How to build a blog with Angular and Scully'
+published: true
+---
+
+# Angular and Scully
--- src/app/articles/articles.component.ts
+++ src/app/articles/articles.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ScullyRoute, ScullyRoutesService } from '@scullyio/ng-lib';
+import { Observable, map } from 'rxjs';
 
 @Component({
   selector: 'app-articles',
   templateUrl: './articles.component.html',
-  styleUrls: ['./articles.component.scss']
+  styleUrls: ['./articles.component.scss'],
 })
-export class ArticlesComponent {
+export class ArticlesComponent implements OnInit {
+  posts$: Observable<ScullyRoute[]> | undefined;
 
+  constructor(private scullySerrvice: ScullyRoutesService) {}
+
+  ngOnInit(): void {
+    this.posts$ = this.scullySerrvice.available$.pipe(map((posts) => posts.filter((post) => post.title)));
+  }
 }
